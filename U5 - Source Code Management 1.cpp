310c52a4bc2b0bd7310c14bb151363b21f7b038d
Shivam Vyas <Shivam_Vyas2006@hotmail.com>
2024-02-26 10:55:15 -0500

    This is my first change for demonstarting commit and sync feature from Visual Studio 2022 Git Changes tab.

--- U5 - Source Code Management 1.cpp
+++ U5 - Source Code Management 1.cpp
@@ -3,5 +3,6 @@ using namespace std;
 
 int main() {
 	cout << "Hello World!" << endl;
+	cout << "This is my first change for demonstarting commit and sync feature from Visual Studio 2022 Git Changes tab." << endl;
 	return 0;
 }
